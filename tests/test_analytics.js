92c4fbd53b43f7ef453ccb95bb8648fb965dd940
titoneedsakidney <roberto.erb.92@gmail.com>
2026-09-01 20:05:34 -0400

    test: cover semantic analytics event contract

--- tests/test_analytics.js
+++ tests/test_analytics.js
@@ -15,37 +15,57 @@ global.gtag = (...args) => events.push(args);
 
 require('../scripts/analytics.js');
 
-const trackedLink = {
-  textContent: 'Buy the Paperback',
-  href: 'https://www.amazon.com/dp/B0DSXVL84P',
-  getAttribute(name) {
-    return {
-      'data-evt': 'book_en_paperback',
-      'data-evt-loc': 'book_page'
-    }[name] || null;
-  },
-  closest() {
-    return null;
-  }
-};
-
-clickHandler({
-  target: {
-    closest(selector) {
-      return selector === 'a[data-evt]' ? trackedLink : null;
+function clickLink(id, location = 'book_page') {
+  const trackedLink = {
+    getAttribute(name) {
+      return {
+        'data-evt': id,
+        'data-evt-loc': location
+      }[name] || null;
+    },
+    closest() {
+      return null;
     }
-  }
-});
+  };
+  clickHandler({
+    target: {
+      closest(selector) {
+        return selector === 'a[data-evt]' ? trackedLink : null;
+      }
+    }
+  });
+}
 
+clickLink('book_en_paperback');
+assert.deepEqual(events, [
+  [
+    'event',
+    'cta_click',
+    { cta_id: 'book_en_paperback', cta_loc: 'book_page' }
+  ],
+  [
+    'event',
+    'outbound_purchase',
+    {
+      action_id: 'book_en_paperback',
+      action_location: 'book_page',
+      content_language: 'en',
+      item_format: 'paperback'
+    }
+  ]
+]);
+
+events.length = 0;
+clickLink('professional_en', 'homepage');
 assert.deepEqual(events, [[
   'event',
   'cta_click',
-  {
-    cta_id: 'book_en_paperback',
-    cta_text: 'Buy the Paperback',
-    cta_loc: 'book_page',
-    link_url: 'https://www.amazon.com/dp/B0DSXVL84P'
-  }
+  { cta_id: 'professional_en', cta_loc: 'homepage' }
 ]]);
+
+events.length = 0;
+clickLink('book_es_info', 'site_footer');
+assert.equal(events[1][1], 'view_book');
+assert.equal(events[1][2].content_language, 'es');
 
 console.log('analytics event contract: pass');
